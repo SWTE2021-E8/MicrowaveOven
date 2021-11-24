4c335688e47c8c38593b9c2a35740a3f67b9d19f
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-11-24 12:20:49 +0100

    Modified integration tests

--- Microwave.Test.Integration/TDStep2.cs
+++ Microwave.Test.Integration/TDStep2.cs
@@ -97,24 +97,15 @@ namespace Microwave.Test.Integration
             output.Received().OutputLine(Arg.Is<string>(str => str.Contains("150 W")));
         }
 
-<<<<<<< HEAD
         [Test]
         public void UI_Display_Showtime_1min()
         {
             powerButton.Press();
             timeButton.Press();
 
-=======
-        [Test]
-        public void UI_Display_ShowPower_700W()
-        {
-            powerButton.Press();
-            powerDial.Dial(700);
->>>>>>> 581ab8b1c6f4911735cc2737a997424da8fa3466
             output.Received().OutputLine(Arg.Is<string>(str => str.Contains("01:00")));
         }
 
-<<<<<<< HEAD
         [Test]
         public void UI_Display_Showtime_2min()
         {
@@ -231,131 +222,3 @@ namespace Microwave.Test.Integration
     }
 }
 
-=======
-        [Test]
-        public void UI_Display_Showtime_1min()
-        {
-            powerButton.Press();
-            timeButton.Press();
-
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("01:00")));
-        }
-
-        [Test]
-        public void UI_Display_Showtime_2min()
-        {
-            powerButton.Press();
-            timeButton.Press();
-            timeButton.Press();
-
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("02:00")));
-        }
-
-        [Test]
-        public void UI_Display_Clear_()
-        {
-            powerButton.Press();
-            timeButton.Press();
-            startCancelButton.Press();
-
-            // Simulate cooking is done
-            // This would be fine, as we are investigating the UI - Display interface
-            // not anything else
-            // ui.CookingIsDone();
-
-            // Alternative, use the timer as driver for cooker, which 
-            // will make cooking stop
-            // This would be more top down
-            timer.Expired += Raise.EventWith(this, EventArgs.Empty);
-
-            output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("cleared")));
-        }
-
-        #endregion
-
-        #region UI_CookController
-
-        [Test]
-        public void UI_CookController_StartCooking_1min()
-        {
-            powerButton.Press();
-            timeButton.Press();
-            startCancelButton.Press();
-
-            // Cooking has started
-            // Can be verified by timer
-            timer.Received().Start(60);
-        }
-
-        [Test]
-        public void UI_CookController_StartCooking_2min()
-        {
-            powerButton.Press();
-            timeButton.Press();
-            timeButton.Press();
-            startCancelButton.Press();
-
-            // Cooking has started
-            // Can be verified by timer
-            timer.Received().Start(120);
-        }
-
-        [Test]
-        public void UI_CookController_StartCooking_50W()
-        {
-            powerButton.Press();
-            timeButton.Press();
-            startCancelButton.Press();
-
-            // Cooking has started
-            // Can be verified by powertube
-            powerTube.Received().TurnOn(50);
-        }
-
-        [Test]
-        public void UI_CookController_StartCooking_100W()
-        {
-            powerButton.Press();
-            powerDial.Dial(100);
-            timeButton.Press();
-            startCancelButton.Press();
-
-            // Cooking has started
-            // Can be verified by powertube
-            powerTube.Received().TurnOn(100);
-        }
-
-        [Test]
-        public void UI_CookController_StartCooking_700W()
-        {
-
-                powerButton.Press();
-                powerDial.Dial(700);
-
-                timeButton.Press();
-                startCancelButton.Press();
-
-                // Cooking has started
-                // Can be verified by powertube
-                powerTube.Received().TurnOn(700);
-            }
-
-            [Test]
-            public void UI_CookController_Stop()
-            {
-                powerButton.Press();
-                timeButton.Press();
-                startCancelButton.Press();
-
-                // Now we force stopping
-                startCancelButton.Press();
-
-                // Cook controller should have stopped powertube
-                powerTube.Received(1).TurnOff();
-            }
-
-            #endregion
-        }
-    }
-
->>>>>>> 581ab8b1c6f4911735cc2737a997424da8fa3466
